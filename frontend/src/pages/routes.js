899325957ca8abd8927ef67b01f56000927ce3ec
github username <github email address>
2019-04-15 10:49:02 -0400

    started on creating login

--- frontend/src/pages/routes.js
+++ frontend/src/pages/routes.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Switch, Route, Redirect } from 'react-router-dom'
 import UsernameRoutes from './username/_gifts/routes'
+import LoginPage from './login'
 
 class Routes extends React.Component {
   render() {
@@ -13,9 +14,15 @@ class Routes extends React.Component {
           render={() => <Redirect to={`/${this.props.name}/gifts`}/>}
         />
         <Route
+          exact
+          path='/login'
+          render={<div>login</div>}
+        />
+        <Route
           path='/:username'
           render={({match}) => <UsernameRoutes match={match}/>}
         />
+
       </Switch>
     )
   }
