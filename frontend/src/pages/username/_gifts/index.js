36cbd8f3e16ff873eb19f20b068781801f25a754
github username <github email address>
2019-04-16 19:47:19 -0400

    stylize gift index

--- frontend/src/pages/username/_gifts/index.js
+++ frontend/src/pages/username/_gifts/index.js
@@ -36,12 +36,12 @@ class Gifts extends React.Component {
   render () {
     console.log(this.props.userObj.gifts)
     return (
-      <div className="uk-position-relative uk-visible-toggle uk-light" data-tabindex="-1" data-uk-slider>
+      <div id='slider-div' className="uk-position-relative uk-visible-toggle uk-light" data-tabindex="-1" data-uk-slider>
         <ul className="uk-slider-items uk-child-width-1-2 uk-child-width-1-3@s uk-child-width-1-3@m">
           {!!this.props.userObj.gifts && this.makeGifts()}
         </ul>
-        <a id='slider' className="uk-position-center-left uk-position-small" href="#" data-uk-slidenav-previous data-uk-slider-item="previous"></a>
-        <a id='slider' className="uk-position-center-right uk-position-small" href="#" data-uk-slidenav-next data-uk-slider-item="next"></a>
+        <span id='slider' className="uk-position-center-left uk-position-small" uk-icon="icon:  triangle-left; ratio: 2" data-uk-slidenav-previous data-uk-slider-item="previous"></span>
+        <span id='slider' className="uk-position-center-right uk-position-small" uk-icon="icon:  triangle-right; ratio: 2" data-uk-slidenav-next data-uk-slider-item="next"></span>
       </div>
     )
   }
