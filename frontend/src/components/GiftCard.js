7afa4899751b2ba9e1d1be031433f8f61aed9f81
github username <github email address>
2019-04-17 12:45:32 -0400

    added a break in between name and date

--- frontend/src/components/GiftCard.js
+++ frontend/src/components/GiftCard.js
@@ -8,7 +8,7 @@ const GiftCard = (props) => {
         <li className='uk-text-center'>
           <img src={require("../img/open-box-with-two-hearts2.png")} onClick={() => props.handleClick(props.gift)}/>
             <hr />
-            <p className='uk-panel gift-text'>From: {props.gift.sender_id}, <i>({props.gift.date})</i></p>
+            <p className='uk-panel gift-text'>From: {props.gift.sender_id}, <br/><i>({props.gift.date})</i></p>
         </li>
       )
     } else {
@@ -16,7 +16,7 @@ const GiftCard = (props) => {
         <li className='uk-text-center'>
           <img className='gift-box' src={require("../img/gift2.png")} onClick={() => props.handleClick(props.gift)}/>
           <hr />
-          <p className='uk-panel gift-text'>From: {props.gift.sender_id}, <i>({props.gift.date})</i></p>
+          <p className='uk-panel gift-text'>From: {props.gift.sender_id}, <br/><i>({props.gift.date})</i></p>
         </li>
       )
     }
